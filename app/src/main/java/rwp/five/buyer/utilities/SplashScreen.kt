5fc3f11261b24536a6748d3c4ca1a74adab4a4cf
udeeshabandara@gmail.com <udeesha@wondereng.com>
2022-05-23 18:05:00 +0530

    change main logo

--- app/src/main/java/rwp/five/buyer/utilities/SplashScreen.kt
+++ app/src/main/java/rwp/five/buyer/utilities/SplashScreen.kt
@@ -12,8 +12,8 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tinyDB = TinyDB(applicationContext)
-        tinyDB.putString("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoibWFyY29zLmhlbnJpLmNvbWEiLCJmY20iOiJhcyIsIm5hbWUiOiJ1bmRlZmluZWQgdW5kZWZpbmVkIiwicmVmcmVzaEtleSI6ImhMR2wwT3dhRFliQ01tYjcrYXhlZWc9PSIsImlhdCI6MTY1MTk5MDA5MX0.NqoSfgsfelHHfNQm0s7uSS6tUzMjaOWcnGCGJp9Fza4")
-        if (!tinyDB.getBoolean("isLogged"))
+//        tinyDB.putString("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoibWFyY29zLmhlbnJpLmNvbWEiLCJmY20iOiJhcyIsIm5hbWUiOiJ1bmRlZmluZWQgdW5kZWZpbmVkIiwicmVmcmVzaEtleSI6ImhMR2wwT3dhRFliQ01tYjcrYXhlZWc9PSIsImlhdCI6MTY1MTk5MDA5MX0.NqoSfgsfelHHfNQm0s7uSS6tUzMjaOWcnGCGJp9Fza4")
+        if (tinyDB.getBoolean("isLogged"))
             startActivity(Intent(this, Main::class.java))
         else
             startActivity(Intent(this, LoginActivity::class.java))
